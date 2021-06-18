1a5852e0f4ded7208c4f7620383aabc4b506dae1
EmilyLow <emlow@me.com>
2021-06-18 14:51:23 -0500

    Added new functioning algorithm

--- src/components/utils/organization.js
+++ src/components/utils/organization.js
@@ -1,11 +1,11 @@
 
 let colOffset = 2;
 
-//TODO: Figure out where in the algorithm "placement" occurs
-//TODO: Draw out algorithm on paper
+
 
 //Note: This assumes all events will be on same day
 function organizeEvents(rawEvents, dayNum) {
+
   
     if(rawEvents.length === 0) {
         return [];
@@ -16,7 +16,7 @@ function organizeEvents(rawEvents, dayNum) {
     rawEvents.forEach(event => {
         let newEvent = {...event, start_col: 0, span: 0};
         cleanEvents.push(newEvent);
-
+        
     });
 
     let columns = [];
@@ -44,22 +44,34 @@ function organizeEvents(rawEvents, dayNum) {
             //Clears columns for next event set
             columns = [];
             lastEventEnd = null;
+           
         }
 
+     
         let eventPlaced = false;
 
 
-    
+        
         for (let i = 0; i < columns.length; i++) {
             //Assuming empty columns are possible
+           
             let last = columns[i][columns[i].length - 1];
 
-            //ToDo: Define physical place so it can be checked
+            
 
-            if(last !== undefined && !checkPhysicalInt(last, currentEvent)) {
-                //Alter currentEvent to have matching physical position
+
+            if(last !== undefined && !checkTimeInt(last, currentEvent)) {
+               
+                
+       
+                currentEvent.span = 1;
+                currentEvent.start_col = i;
+
                 columns[i].push(currentEvent);
                 eventPlaced = true;
+                
+
+                
 
                 break;
             }
@@ -67,131 +79,183 @@ function organizeEvents(rawEvents, dayNum) {
 
         if(!eventPlaced) {
             
-            //TODO: Alter currentEvent to have matching physical position
+        
+            currentEvent.span = 1;
+         
+ 
+            currentEvent.start_col = columns.length;
+          
             columns.push([currentEvent]);
         }
 
         if(lastEventEnd === null || new Date(currentEvent.end_time) > lastEventEnd ) {
             lastEventEnd = new Date(currentEvent.end_time);
         }
-       
-        if (columns.length > 0) {
-
-            packEvents(columns, dayNum);
-
-        }
 
 
     }
 
-    let organizedEvents = [];
 
-    //Places events in column inside new array
-    //This may not be necessary. Depends on if I alter them in place or not. (Better to not?)
-    for (let i = 0; i < columns.length; i++) {
-        for(let j = 0; j < columns[i].length; j++) {
+    if (columns.length > 0) {
 
-            organizedEvents.push(columns[i][j]);
-
-        }
+        packEvents(columns, dayNum);
 
     }
-    return organizedEvents;
+
+
+ 
+    return sortedEvents;
 
 }
 
 function packEvents(columns, dayNum) {
-
-    let colCount = columns.length;
+   
+    let numColumns = columns.length;
 
 
     //Iterates through each column
-    for (let i = 0; i < colCount; i++) {
+    for (let i = 0; i < numColumns; i++) {
         //Iterates through each event in the column
-        // console.log("columns[i]", columns[i].length);
+    
         for(let j = 0; j < columns[i].length; j++) {
 
-            //ToDo: Unsure of this
-            //Remember! Example ends up with percentages
-            // let startPos = i + (12/colCount) + baseColumn;
 
-            // let copyEvent = {...columns[i][j], start_col: startPos};
-            // console.log("CopyEvent", copyEvent);
+            //Attempt
+            //This attempt means the more elegant parts of this algorithm only work if events < 4, such as the parts to fill empty space
+            //Can this be fixed?
+            let colAdjust = 2 + dayNum*12;
+            let currentEvent = columns[i][j];
+           
+            if(numColumns <= 4) {
+                
+                let colSpan = expandEvent(currentEvent, i, columns);
+           
+   
+                currentEvent.start_col = (i/numColumns) * 12 + colAdjust;
+                currentEvent.span = (colSpan/numColumns) * 12;
+            }
+            else if(numColumns > 4 && numColumns <= 6) {
+                currentEvent.span = 2;
+                currentEvent.start_col = 2 * i + colAdjust;
+            } else if (numColumns > 6 && numColumns < 13) {
+                currentEvent.span = 1;
+                currentEvent.start_col = 1 * i + colAdjust;
+            } else {
+                console.log("num columns", numColumns);
+                console.log("ERROR: No more than 12 overlapping events");
+            }
 
-
-            //TODO: An event should have a defined position by the time it reaches here. Check this. 
-            //But position should be preliminary
-            //Now, set actual positions based off number of columns
-            //Then, use colSpan to set expanded positions based off that
-
-            let colSpan = expandEvent(columns[i][j], i, columns);
-            
-
-            //Update actual positions 
 
 
         }
     }; 
 }
 
+
+
 function expandEvent(event, iColumn, columns) {
     let colSpan = 1;
 
-    //Increase span of event to match colSpan as it goes.
-    //It should be expanding to the right I believe?
 
-   
+
     //Iterates through each column
     for (let i = iColumn + 1; i < columns.length; i++) {
-        //Iterates through each event in the column
-        for(let j = 0; j < columns[i].length; i++) {
 
+        for(let j = 0; j < columns[i].length; j++) {
           
             let comEvent = columns[i][j];
 
+            
 
             if(checkPhysicalInt(event, comEvent)) {
+
                 return colSpan;
             }
+
         }
-    
+
+        
         colSpan++;
-        //Change event to update to new span (Here I think???)
-        //Or should I make a test event to compare to?
-        // event.span = colSpan;
+        event.span = colSpan;
+
     }
 
-    if(colSpan >= 12) {
-        console.log("Error: No more than twelve intersecting events");
-        //Throw actual error here
-    }
-    // console.log(colSpan);
+
     return colSpan;
 
 }
 
+function checkTimeInt(event1, event2) {
+    let maxStart = max(event1.start_time, event2.start_time);
 
+   let end1Copy = new Date(event1.end_time);
+   let end2Copy = new Date(event2.end_time);
+
+    //Bumping back by one minute in calculations, to prevent intersections when things start and end in same hour
+    //Converted to new Date for readability when logging
+    let altEnd1 = new Date(end1Copy.setMinutes(end1Copy.getMinutes() -1));
+    let altEnd2 = new Date(end2Copy.setMinutes(end2Copy.getMinutes() -1));
+
+    let minEnd = min(altEnd1, altEnd2);
+
+    
+
+    if(maxStart <= minEnd) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Find why this isn't working, and never returns true
+//I think maybe it also needs to check for time intersection?
 function checkPhysicalInt(event1, event2) {
 
-    //Error check
-    if(event1.start_col === 0 || event2.start_col === 0 || event1.span === 0 || event2.span === 0) {
+
+    // if(event1.id === 1073) {
+    //     console.log("--------")
+    //     console.log("Event1", event1.event_name);
+    //     console.log("Event2", event2.event_name);
+    // }
+    if(event1.span === 0 || event2.span === 0) {
         console.log("Error! Undefined physical placement when checking intersection");
     }
 
+    if(checkTimeInt(event1, event2) === false) {
+        // if(event1.id === 1073) console.log("Time false")
+        // console.log("Time false");
+        return false;
+    } else {
 
-        //Subtracted by 0.1 so shared boundaries don't count as intersection.
-        let end_col1 = event1.start_col + event1.span - 0.1;
-        let end_col2 = event2.start_col + event2.span - 0.1;
+                //Subtracted by 0.1 so shared boundaries don't count as intersection.
+                //Removed .01 subtraction to test
+                let end_col1 = event1.start_col + event1.span;
+                let end_col2 = event2.start_col + event2.span;
+        
+                let maxStart = max(event1.start_col, event2.start_col);
+                let minEnd = min(end_col1, end_col2);
+                // if(event1.id === 1073) console.log("maxStart", maxStart);
+                // if(event1.id === 1073) console.log("minEnd", minEnd);
 
-        let maxStart = max(event1.start_col, event2.start_col);
-        let minEnd = min(end_col1, end_col2);
+                // if(event1.id === 1073) {
+                //     console.log("Event 1 start_col",event1.start_col );
+                //     console.log("Event 1 end col", end_col1);
+                //     console.log("Event 2 start_col",event2.start_col );
+                //     console.log("Event 2 end col", end_col2);
+                // }
+        
+                if(maxStart <= minEnd) {
+                    // console.log("True");
+                    // if(event1.id === 1073) console.log("True")
+                    return true;
+                } else {
+                    // if(event1.id === 1073) console.log("False")
+                    // console.log("False");
+                    return false;
+                }
+    }
 
-        if(maxStart <= minEnd) {
-         
-            return true;
-        } else {
-            return false;
-        }
+
   }
 
   function max(value1, value2) {
