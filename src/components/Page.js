47c7035a8bff01ba69f575939ef932e988cc483c
EmilyLow <emlow@me.com>
2021-07-28 16:51:31 -0500

    Clear button does not cause reload but updates data

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,6 +2,7 @@
 import ScheduleHolder from "./scheduleComp/ScheduleHolder";
 import {useContext} from "react";
 import UserContext from "./utils/UserContext";
+import axios from 'axios';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -44,9 +45,10 @@ const useStyles = makeStyles( theme => ({
       },
 
       clearButton: {
-          maxWidth: '150px',
+          maxWidth: '100px',
           maxHeight: '100px',
           padding: '10px',
+          marginRight: '50px',
           color: theme.palette.secondary.main,
           backgroundColor: theme.palette.primary.main
       }
@@ -59,27 +61,54 @@ export default function Page(props) {
     const userData = useContext(UserContext);
     let scheduleId = props.scheduleId;
 
-    const classes = useStyles();
-
-    let clearButton = <></>; 
-
-  if(scheduleId === 0) {
-      clearButton = <Button  variant="contained" className={classes.clearButton}>Clear Schedule</Button>
-  }
-
     if(scheduleId === 0) {
       
         scheduleId = userData.currentUser.scheduleId;
     }
 
+    let personalSchedule = false;
+
+    if(props.scheduleId === 0) {
+        personalSchedule = true;
+    }
+
+    const classes = useStyles();
+
+  
+
+    //Works but needs to trigger visual reload. 
+    const clearData = () => {
+       
+        console.log(scheduleId);
+        if(scheduleId === 0) {
+            //Guest user, local storage
+            localStorage.setItem("guestEvents", JSON.stringify([]));
+        } else {
+            //Logged in user, backend
+            
+            axios.delete(props.url + "/schedules/" + scheduleId + "/events")
+            .then((response) => {
+                
+            })
+            .catch(error => console.error(`Error: ${error}`))
+        }
+    }
+
+    let clearButton = <></>; 
+
+  if(personalSchedule) {
+      clearButton = <Button onClick={clearData}  variant="contained" className={classes.clearButton}>Clear Schedule</Button>
+  }
+
     if(props.sizeMult <= 1) {
         return(
             <div className={classes.mainDivNormal}>
-                <div>
+               
+               <div>
                 {clearButton}
                 </div>
-                
                 <ScheduleHolder scheduleId = {scheduleId} sizeMult={props.sizeMult} url={props.url}/>
+              
             </div>
         ) 
     } else {
